f6c2bc027daa362ec92a6674d4c2bfd57e17dfd4
rolandoworks <rolandoworks@gmail.com>
2022-05-23 20:20:30 -0400

    demo-first commit

---
+++ src/main/java/com/example/demokotlin/DemoKotlinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demokotlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKotlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKotlinApplication.class, args);
+	}
+
+}
